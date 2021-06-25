6084b561c7d0f321a934f50a4663687b9e7410c5
D3C0RU5 <chpeterle.profissional@gmail.com>
2021-06-25 15:07:29 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+import { Input, Text,Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
+
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  const entrar = () => {
+    console.log("Entrou")
+    Alert.alert("Entrou","Login acessado")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text h3>Entre no TemTudaki!</Text>
+      <Input
+        placeholder="E-mail"
+        leftIcon={{ type: "font-awesome", name: "envelope" }}
+        onChangeText={value => setEmail(value)}
+        keyboardType="email-address"
+      />
+      <Input
+        placeholder="Sua senha"
+        leftIcon={{ type: "font-awesome", name: "lock" }}
+        onChangeText={value => setPassword(value)}
+        secureTextEntry={true}
+      />
+      <Button
+        icon={
+          <Icon
+            name="check"
+            size={15}
+            color="white"
+          />
+        }
+        title="Entrar"
+        onPress={() => entrar()}
+      />
     </View>
   );
 }
